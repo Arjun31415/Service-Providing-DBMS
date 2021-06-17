2bd95902edcb17b89d218aa5850560a1d8cac352
Arjun31415 <arjunp0710@gmail.com>
2021-06-17 22:21:31 +0530

    bugs in admin pages

--- employee_pages.py
+++ employee_pages.py
@@ -15,7 +15,7 @@ class Employee:
         self.parent.geometry(str(w)+"x"+str(h))
         self.email = username
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
-        
+
         # pub.subscribe(self.listner, "BookserviceWindowClosed")
         pub.subscribe(self.listner, "EditinfoWindowClosed")
 
@@ -151,6 +151,7 @@ class Employee:
 
 ####################################################################################################################################
 
+
 class Admin:
 
     def __init__(self, master, username):
@@ -206,9 +207,9 @@ class Admin:
 
         # for emp
         ad_label = Label(self.parent, text="Add an Employee:",
-                           font=("Times", 11),
-                           anchor='center'
-                           )
+                         font=("Times", 11),
+                         anchor='center'
+                         )
         ad_label.place(x=25/260*w, y=(h/8)+95)
         ad = Button(self.parent, text='Book')
         ad.place(x=25/150*w, y=(h/8)+120)
@@ -226,9 +227,9 @@ class Admin:
 
         # servi
         serv_label = Label(self.parent, text="Add Service:",
-                          font=("Times", 11),
-                          anchor='center'
-                          )
+                           font=("Times", 11),
+                           anchor='center'
+                           )
         serv_label.place(x=25/185*w, y=(h/8)+170)
         ser = Button(self.parent, text='View')
         ser.place(x=25/150*w, y=(h/8)+195)
@@ -244,28 +245,32 @@ class Admin:
         pro.place(x=25/38*w, y=(h/8)+195)
         pro.bind('<Button-1>', self.remserv)
      # ----------------------------------------------------------------
+
     def addemp(self, event):
         self.hide()
         addemp = Toplevel(self.parent)
-        AddempWindow = Addemp(addemp)
+        AddempWindow = Addemp(master=addemp)
      # ----------------------------------------------------------------
+
     def removeemp(self, event):
         self.hide()
         removeemp = Toplevel(self.parent)
-        RemoveempWindow = Removeemp(removeemp)
+        RemoveempWindow = Removeemp(master=removeemp)
  # ----------------------------------------------------------------
+
     def addserv(self, event):
         self.hide()
         addserv = Toplevel(self.parent)
-        AddservWindow = Addserv(addserv)
+        AddservWindow = Addserv(master=addserv)
 # ----------------------------------------------------------------
+
     def remserv(self, event):
         self.hide()
         remserv = Toplevel(self.parent)
-        RemservWindow = Remserv(remserv)
-    
+        RemservWindow = Remserv(master=remserv)
 
     # ----------------------------------------------------------------
+
     def on_closing(self):
 
         if messagebox.askokcancel("Quit", "Do you want to signout?"):
@@ -296,21 +301,20 @@ class Admin:
         self.show()
     # ----------------------------------------------------------------
 #####################################################################################
+
+
 class Addemp:
 
-    def __init__(self, email, person='e', master=None):
+    def __init__(self, master=None):
         self.parent = master
         self.parent.title('ADD EMPLOYEE')
         self.parent.geometry('500x250')
-        self.email = email
-        print("Email of this dude:", str(self.email))
-        
-        self.make_widgets(person)
+        self.make_widgets()
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     # ----------------------------------------------------------------
 
-    def make_widgets(self, person='c'):
+    def make_widgets(self):
         # put the widgets
         self.parent.title("ADD Emp")
         heading = Label(self.parent, text="NEW EMPLOYEE", font=("Arial", 16))
@@ -319,22 +323,21 @@ class Addemp:
         def reset():
             # Email
             usr_label = Label(self.parent, text="Email:",
-                          font=("Times", 11),
-                          anchor='center'
-                          )
+                              font=("Times", 11),
+                              anchor='center'
+                              )
             usr_label.place(x=25/400*w, y=(h/8))
             self.mail = Entry(self.parent, bg='white', font=("Arail", 8))
             self.mail.place(x=25/135 * w, y=(h/8)+2)
             # Cust_id
             id_label = Label(self.parent, text="Identification No: ",
-                         font=("Times", 11),
-                         anchor='center'
-                         )
+                             font=("Times", 11),
+                             anchor='center'
+                             )
             id_label.place(x=25/400*w, y=(h/8)+23)
             self.id = Entry(self.parent, bg='white', font=("Arail", 8))
             self.id.place(x=25/110 * w, y=(h/8)+25)
 
-        
             # Name
             name_label = Label(self.parent, text="Name:",
                                font=("Times", 11),
@@ -344,7 +347,6 @@ class Addemp:
 
             self.nam = Entry(self.parent, bg='white', font=("Arail", 8))
             self.nam.place(x=25/135 * w, y=(h/8)+45)
-            
 
             # Address
             add_label = Label(self.parent, text="Address: ",
@@ -354,7 +356,7 @@ class Addemp:
             add_label.place(x=25/400*w, y=(h/8)+63)
             self.addr = Entry(self.parent, bg='white', font=("Arail", 8))
             self.addr.place(x=25/120 * w, y=(h/8)+66)
-           
+
             # Mobile Number
             Phone_label = Label(self.parent, text="Phone No:",
                                 font=("Times", 11),
@@ -363,24 +365,20 @@ class Addemp:
             Phone_label.place(x=25/400*w, y=(h/8)+83)
             self.phn = Entry(self.parent, bg='white', font=("Arail", 8))
             self.phn.place(x=25/108 * w, y=(h/8)+86)
-            
 
         def get_changes():
             tb.change_custdetails(cust_id=self.data["ID"],
-                                      name=self.nam.get(),
-                                      address=self.addr.get(),
-                                      mobile=self.phn.get())
+                                  name=self.nam.get(),
+                                  address=self.addr.get(),
+                                  mobile=self.phn.get())
             self.data = tb.get_customer_details(str(self.email))
-            
 
         reset()
         savech = Button(self.parent, text='ADD', command=get_changes)
         savech.place(x=25/41*w, y=(h/8)+130)
-        
 
         canc = Button(self.parent, text='Cancel', command=reset)
         canc.place(x=25/70*w, y=(h/8)+130)
-        
 
     # ------------------------------------------------
 
@@ -413,64 +411,58 @@ class Addemp:
     # ----------------------------------------------------------------
 
 ################################################################################
+
+
 class Removeemp:
 
-    def __init__(self, email, person='c', master=None):
+    def __init__(self, master=None):
         self.parent = master
         self.parent.title('REMOVE EMPLOYEE')
         self.parent.geometry('500x250')
-        self.email = email
-        print("Email of this dude:", str(self.email))
-        
-        self.make_widgets(person)
+
+        self.make_widgets()
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     # ----------------------------------------------------------------
 
-    def make_widgets(self, person='c'):
+    def make_widgets(self):
         # put the widgets
-        self.parent.title("ADD Emp")
-        heading = Label(self.parent, text="REMOVE EMPLOYEE", font=("Arial", 16))
+        self.parent.title("Remove Emp")
+        heading = Label(self.parent, text="REMOVE EMPLOYEE",
+                        font=("Arial", 16))
         heading.pack()
 
         def reset():
             # Email
             usr_label = Label(self.parent, text="Email:",
-                          font=("Times", 11),
-                          anchor='center'
-                          )
+                              font=("Times", 11),
+                              anchor='center'
+                              )
             usr_label.place(x=25/400*w, y=(h/8))
             self.mail = Entry(self.parent, bg='white', font=("Arail", 8))
             self.mail.place(x=25/135 * w, y=(h/8)+2)
             # emp id
             id_label = Label(self.parent, text="Identification No: ",
-                         font=("Times", 11),
-                         anchor='center'
-                         )
+                             font=("Times", 11),
+                             anchor='center'
+                             )
             id_label.place(x=25/400*w, y=(h/8)+23)
             self.id = Entry(self.parent, bg='white', font=("Arail", 8))
             self.id.place(x=25/110 * w, y=(h/8)+25)
 
-        
-            
-            
-
         def get_changes():
             tb.change_custdetails(cust_id=self.data["ID"],
-                                      name=self.nam.get(),
-                                      address=self.addr.get(),
-                                      mobile=self.phn.get())
+                                  name=self.nam.get(),
+                                  address=self.addr.get(),
+                                  mobile=self.phn.get())
             self.data = tb.get_customer_details(str(self.email))
-            
 
         reset()
         savech = Button(self.parent, text='ADD', command=get_changes)
         savech.place(x=25/41*w, y=(h/8)+130)
-        
 
         canc = Button(self.parent, text='Cancel', command=reset)
         canc.place(x=25/70*w, y=(h/8)+130)
-       
 
     # ------------------------------------------------
 
@@ -500,23 +492,22 @@ class Removeemp:
             closes the window and sends a message to the main window
         """
         pub.sendMessage("RemoveempWindowClosed", arg1="data")
-      ##############################################################  
+      ##############################################################
+
+
 class Addserv:
 
-
-    def __init__(self, email, person='c', master=None):
+    def __init__(self, master=None):
         self.parent = master
         self.parent.title('ADD EMPLOYEE')
         self.parent.geometry('500x250')
-        
-        
-        
-        self.make_widgets(person)
+
+        self.make_widgets()
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     # ----------------------------------------------------------------
 
-    def make_widgets(self, person='c'):
+    def make_widgets(self):
         # put the widgets
         self.parent.title("ADD serv")
         heading = Label(self.parent, text="NEW SERVICE", font=("Arial", 16))
@@ -525,22 +516,21 @@ class Addserv:
         def reset():
             # serv name
             usr_label = Label(self.parent, text="Service Name:",
-                          font=("Times", 11),
-                          anchor='center'
-                          )
+                              font=("Times", 11),
+                              anchor='center'
+                              )
             usr_label.place(x=25/400*w, y=(h/8))
             self.mail = Entry(self.parent, bg='white', font=("Arail", 8))
             self.mail.place(x=25/135 * w, y=(h/8)+2)
             # serv_id
             id_label = Label(self.parent, text="Service ID ",
-                         font=("Times", 11),
-                         anchor='center'
-                         )
+                             font=("Times", 11),
+                             anchor='center'
+                             )
             id_label.place(x=25/400*w, y=(h/8)+23)
             self.id = Entry(self.parent, bg='white', font=("Arail", 8))
             self.id.place(x=25/110 * w, y=(h/8)+25)
 
-        
             # cost
             cost_label = Label(self.parent, text="Cost:",
                                font=("Times", 11),
@@ -550,17 +540,13 @@ class Addserv:
 
             self.cost = Entry(self.parent, bg='white', font=("Arail", 8))
             self.cost.place(x=25/135 * w, y=(h/8)+45)
-            
-
-           
 
         def get_changes():
             tb.change_custdetails(cust_id=self.data["ID"],
-                                      name=self.nam.get(),
-                                      address=self.addr.get(),
-                                      mobile=self.phn.get())
+                                  name=self.nam.get(),
+                                  address=self.addr.get(),
+                                  mobile=self.phn.get())
             self.data = tb.get_customer_details(str(self.email))
-            
 
         reset()
         savech = Button(self.parent, text='ADD', command=get_changes)
@@ -602,22 +588,21 @@ class Addserv:
     # ----------------------------------------------------------------
 
 ################################################################################
+
+
 class Remserv:
 
-
-    def __init__(self, email, person='c', master=None):
+    def __init__(self,  master=None):
         self.parent = master
         self.parent.title('REMOVE SERVICE')
         self.parent.geometry('500x250')
-        
-        
-        
-        self.make_widgets(person)
+
+        self.make_widgets()
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     # ----------------------------------------------------------------
 
-    def make_widgets(self, person='c'):
+    def make_widgets(self):
         # put the widgets
         self.parent.title("remove serv")
         heading = Label(self.parent, text="DELETE SERVICE", font=("Arial", 16))
@@ -626,32 +611,27 @@ class Remserv:
         def reset():
             # NAme
             usr_label = Label(self.parent, text="Service Name:",
-                          font=("Times", 11),
-                          anchor='center'
-                          )
+                              font=("Times", 11),
+                              anchor='center'
+                              )
             usr_label.place(x=25/400*w, y=(h/8))
             self.mail = Entry(self.parent, bg='white', font=("Arail", 8))
             self.mail.place(x=25/135 * w, y=(h/8)+2)
             # Cust_id
             id_label = Label(self.parent, text="Service ID ",
-                         font=("Times", 11),
-                         anchor='center'
-                         )
+                             font=("Times", 11),
+                             anchor='center'
+                             )
             id_label.place(x=25/400*w, y=(h/8)+23)
             self.id = Entry(self.parent, bg='white', font=("Arail", 8))
             self.id.place(x=25/110 * w, y=(h/8)+25)
 
-        
-
-           
-
         def get_changes():
             tb.change_custdetails(cust_id=self.data["ID"],
-                                      name=self.nam.get(),
-                                      address=self.addr.get(),
-                                      mobile=self.phn.get())
+                                  name=self.nam.get(),
+                                  address=self.addr.get(),
+                                  mobile=self.phn.get())
             self.data = tb.get_customer_details(str(self.email))
-            
 
         reset()
         savech = Button(self.parent, text='ADD', command=get_changes)
